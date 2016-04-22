35815e344eea87ae43eddd373b47eaff9c2b921f
Adriel Chagay <adriel98@hotmail.com>
2016-04-22 15:54:06 -0300

    Agregado Reglas de Prueba

--- webPage/urls.py
+++ webPage/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'webPage.views.index', name='index'),
+    url(r'reglas-de-prueba/$', 'webPage.views.reglas', name='reglas'),
 )
