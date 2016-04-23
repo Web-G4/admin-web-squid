ea4b36def4e29adafaf6eb9fbec7e1cf826cd4c3
Francisco de Maussion <franciscodemaussion@gmail.com>
2016-04-23 19:31:25 -0300

    Reglas url y base

--- webPage/urls.py
+++ webPage/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'webPage.views.index', name='index'),
-    url(r'reglas-de-prueba/$', 'webPage.views.reglas', name='reglas'),
+    url(r'reglas/$', 'webPage.views.reglas', name='reglas'),
 )
