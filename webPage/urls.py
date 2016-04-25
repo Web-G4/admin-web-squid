15ca4ff82ec294e0deb378ce946c553123d4d044
Adriel Chagay <adriel98@hotmail.com>
2016-04-24 23:14:32 -0300

    Modificando 'Agregar Regla'

--- webPage/urls.py
+++ webPage/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'webPage.views.index', name='index'),
     url(r'reglas/$', 'webPage.views.reglas', name='reglas'),
+    url(r'addReglas/$', 'webPage.views.addReglas', name='addReglas'),
 )
