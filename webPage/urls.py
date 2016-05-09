5dfdcd13443ebf216ce1db3268055c266bd0c580
satissera <piojotissera@gmail.com>
2016-05-09 15:56:47 -0300

    nombres de los privilegios en las tab (no funcional, falta que tire las rulelist en cada tab)

--- webPage/urls.py
+++ webPage/urls.py
@@ -3,4 +3,5 @@ urlpatterns = patterns('',
     url(r'^$', 'webPage.views.index', name='index'),
     url(r'reglas/$', 'webPage.views.reglas', name='reglas'),
     url(r'addReglas/$', 'webPage.views.addReglas', name='addReglas'),
+#    url(r'^listRules/(?P<idLista>[0-9]+)/$', 'webPage.views.reglas', name='listRule'),
 )
--- webPage/views.py
+++ webPage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response, render, redirect
 from django.template import RequestContext
-from webPage.models import Rule
+from webPage.models import Rule, Rulelist, Privilege
 
 # Create your views here.
 def index(request):
@@ -10,9 +10,14 @@ def index(request):
 def reglas(request):
     context = RequestContext(request)
     reglas = Rule.objects.all()
-    return render_to_response('reglas.html',{'reglas':reglas},
+    privileges = Privilege.objects.all()
+    #lista = RuleList.getObject(id=idLista)
+    #reglas = lista.ruleasigned
+    return render_to_response('reglas.html',{'reglas':reglas, 'privileges':privileges},
                               context)
 def addReglas(request):
     context = RequestContext(request)
     return render_to_response('addReglas.html',
                               context)
+
+
