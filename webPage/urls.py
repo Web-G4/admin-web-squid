4df6a10b0990b2631172b2272b59f6902a49689c
Francisco de Maussion <franciscodemaussion@gmail.com>
2016-05-18 18:12:01 -0300

    Lista de privilegios + Agregar privilegios

--- webPage/urls.py
+++ webPage/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from webPage import views
 
 urlpatterns = [
-    url(r'^$', views.listaDeUsuarios, name='index'),
-    url(r'reglas/$', views.reglas, name='reglas'),
+    url(r'^$', views.listUsuarios, name='listUsuarios'),
+    url(r'listReglas/$', views.listReglas, name='listReglas'),
+    url(r'listPrivilegios/$', views.listPrivilegios, name='listPrivilegios'),
     url(r'addReglas/$', views.addReglas, name='addReglas'),
-    url(r'addUsuario/$', views.registro, name='registro'),
+    url(r'addUsuario/$', views.addUsuario, name='addUsuario'),
     #url(r'listaDeUsuarios/$', views.listaDeUsuarios, name='listaDeUsuarios'),
 ]
--- webPage/views.py
+++ webPage/views.py
@@ -2,28 +2,39 @@ from django.shortcuts import render_to_response, render, redirect
 from django.template import RequestContext
 from webPage.models import Rule, Rulelist, Privilege, Surfer
 
-def reglas(request):
+def listReglas(request):
     context = RequestContext(request)
     reglas = Rule.objects.all()
     privileges = Privilege.objects.all()
     #lista = RuleList.getObject(id=idLista)
     #reglas = lista.ruleasigned
-    return render_to_response('reglas.html',{'reglas':reglas, 'privileges':privileges},
-                              context)
+    return render_to_response('listReglas.html',{'reglas':reglas, 'privileges':privileges},context)
+
 def addReglas(request):
     context = RequestContext(request)
-    return render_to_response('addReglas.html',
-                              context)
+    return render_to_response('addReglas.html',context)
 
-def registro(request):
+def addUsuario(request):
     context = RequestContext(request)
-    users = Rule.objects.all()
-    return render_to_response('registro.html', {'users':users},
-                              context)
+    privileges = Privilege.objects.all()
+    return render_to_response('addUsuario.html',{'privilegios':privileges},context)
 
-def listaDeUsuarios(request):
+def listUsuarios(request):
     context = RequestContext(request)
     usuarios = Surfer.objects.all()
-    return render_to_response('listaDeUsuarios.html',{'usuarios':usuarios},context)
+    return render_to_response('listUsuarios.html',{'usuarios':usuarios},context)
+
+def listPrivilegios(request):
+    context = RequestContext(request)
+    privileges = Privilege.objects.all()
+    if request.method=='POST':
+        priv = Privilege()
+        priv.nameprivilege = request.POST['p_nom']
+        if request.POST.getlist('p_is')==[u'1']:
+            priv.isblock = True
+        else:
+            priv.isblock = False
+        priv.save()
+    return render_to_response('listPrivilegios.html',{'privilegios':privileges},context)
 
 
