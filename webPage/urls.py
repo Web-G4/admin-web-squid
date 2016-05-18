2f4cae0eeda24a2bbbd4c6697c74dfc2e3814b6e
Francisco de Maussion <franciscodemaussion@gmail.com>
2016-05-18 17:15:57 -0300

    Grafica correcta

--- webPage/urls.py
+++ webPage/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import patterns, include, url
 from webPage import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.listaDeUsuarios, name='index'),
     url(r'reglas/$', views.reglas, name='reglas'),
     url(r'addReglas/$', views.addReglas, name='addReglas'),
-    url(r'listaDeUsuarios/$', listaDeUsuarios, name='listaDeUsuarios'),
     url(r'addUsuario/$', views.registro, name='registro'),
-    #    url(r'^listRules/(?P<idLista>[0-9]+)/$', 'webPage.views.reglas', name='listRule'),
+    #url(r'listaDeUsuarios/$', views.listaDeUsuarios, name='listaDeUsuarios'),
 ]
--- webPage/views.py
+++ webPage/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render_to_response, render, redirect
 from django.template import RequestContext
 from webPage.models import Rule, Rulelist, Privilege, Surfer
 
-# Create your views here.
-def index(request):
-    context = RequestContext(request)
-    return render_to_response('index.html',
-                              context)
 def reglas(request):
     context = RequestContext(request)
     reglas = Rule.objects.all()
@@ -29,11 +24,6 @@ def registro(request):
 def listaDeUsuarios(request):
     context = RequestContext(request)
     usuarios = Surfer.objects.all()
-    privilegios = Privilege.objects.all()
-    if request.method == "POST":
-        id=request.POST.get('id')
-        #suario_aux= Surfer.objects.get(id= id)
-        print("MODIFICA LA BASE DE DATOS")
-    return render_to_response('listaDeUsuarios.html',{'usuarios':usuarios,'privilegios':privilegios},context)
+    return render_to_response('listaDeUsuarios.html',{'usuarios':usuarios},context)
 
 
