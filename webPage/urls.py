adbc89ee3e03b7c585c602ad501d047f4eee556a
Francisco de Maussion <franciscodemaussion@gmail.com>
2016-05-18 17:03:10 -0300

    Base arreglada

--- webPage/urls.py
+++ webPage/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'reglas/$', views.reglas, name='reglas'),
     url(r'addReglas/$', views.addReglas, name='addReglas'),
-    url(r'listaDeUsuarios/$', 'webPage.views.listaDeUsuarios', name='listaDeUsuarios'),
+    url(r'listaDeUsuarios/$', listaDeUsuarios, name='listaDeUsuarios'),
     url(r'addUsuario/$', views.registro, name='registro'),
     #    url(r'^listRules/(?P<idLista>[0-9]+)/$', 'webPage.views.reglas', name='listRule'),
 ]
